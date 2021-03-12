6c0f672568e41cee30036ddce0edcfe2cefbc8a8
Bryan Vazquez <bryanvaz14@gmail.com>
2021-03-11 22:44:59 -0700

    Initial Commit

---
+++ SafeVet/SafeVetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeVetApp.swift
+//  SafeVet
+//
+//  Created by Bryan  Vázquez Hernández on 11/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SafeVetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
